878005005eee929372bb3491aa3397137c3d101c
Jack Yu <jackchungchiehyu@gmail.com>
2017-12-31 13:10:33 +0800

    Check if a method is defined in derived class
    How does the author of a base class ensure that a method
    called by the base class is defined in the derived class,
    without access to the code of the derived class?
    
    This commit uses the __build_class__ function, which
    corresponds to the top-level `class` function, for building
    a class in Python.  The function can be modified by the
    author of the base class, so that, when the base class'
    module is imported into the derived class' module,
    `class` checks whether the said method is defined in the
    derived class.

--- metaclass/library.py
+++ metaclass/library.py
@@ -1,6 +1,19 @@
 
 
 class Base:
-    pass
     def foo(self):
-        return 'foo'
+        return self.bar()
+
+old_bc = __build_class__
+
+def my_bc(func, name, base=None, **kw):
+    if base == Base:
+        print('check if bar method is defined')
+        # Not sure how to acutally check if the bar method
+        # is defined in the derived class though
+    if base is not None:
+        return old_bc(func, name, base, **kw)
+    return old_bc(func, name, **kw)
+
+import builtins
+builtins.__build_class__ = my_bc
--- metaclass/user.py
+++ metaclass/user.py
@@ -2,10 +2,6 @@
 
 from library import Base
 
-
-assert hasattr(Base, 'foo'), 'you broke it, you fool'
-
-
 class Derivied(Base):
     def bar(self):
-        return self.foo()
+        return 'bar'
